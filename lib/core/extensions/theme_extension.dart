82765c6c58813e9390a09e09718e1279fcb72634
Ali ElDalley <lyaldaly881@gmail.com>
2026-08-31 18:29:15 +0200

    feat(theme): add colors and fonts to BuildContext extension

--- lib/core/extensions/theme_extension.dart
+++ lib/core/extensions/theme_extension.dart
@@ -1,7 +1,23 @@
-import 'package:countdown_timer/core/theme/app_icon_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:countdown_timer/core/theme/app_colors.dart';
+import 'package:countdown_timer/core/theme/app_icon_extension.dart';
 
 extension AppThemeContext on BuildContext {
   IconData get themeIcon =>
       Theme.of(this).extension<AppIconExtension>()!.themeToggleIcon;
+
+  Color get primary => Theme.of(this).colorScheme.primary;
+  Color get secondary => Theme.of(this).colorScheme.secondary;
+  Color get tertiary => Theme.of(this).colorScheme.tertiary;
+  Color get surface => Theme.of(this).colorScheme.surface;
+  Color get onSurface => Theme.of(this).colorScheme.onSurface;
+  Color get background => Theme.of(this).scaffoldBackgroundColor;
+  Color get neutral => AppColors.neutral; // ثابت، مو جزء من ColorScheme
+
+  TextStyle? get headline => Theme.of(this).textTheme.headlineLarge;
+  TextStyle? get headlineMedium => Theme.of(this).textTheme.headlineMedium;
+  TextStyle? get body => Theme.of(this).textTheme.bodyLarge;
+  TextStyle? get bodyMedium => Theme.of(this).textTheme.bodyMedium;
+  TextStyle? get countdownNumbers => Theme.of(this).textTheme.displayLarge; // أرقام العداد
+  TextStyle? get label => Theme.of(this).textTheme.labelSmall;
 }
